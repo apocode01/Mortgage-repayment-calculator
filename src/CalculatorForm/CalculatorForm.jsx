d9c3800f95080e5cbff29d7220b821da49f7a8bd
apocode01 <apopanou@gmail.com>
2026-09-08 17:05:28 +0300

    starting to style calculator form

--- src/CalculatorForm/CalculatorForm.jsx
+++ src/CalculatorForm/CalculatorForm.jsx
@@ -3,12 +3,14 @@ import styles from "./CalculatorForm.module.scss"
 function CalculatorForm() {
     return (
         <div className={styles.calculator_form_wrapper}>
-            <div>
-                <h1>Mortgage Calculator</h1>
-                <span>Clear All</span>
+            <div className={styles.calculator_header}>
+                <h1 className={styles.calculator_title}>Mortgage Calculator</h1>
+                <button className={styles.calculator_clear_btn}>
+                    <span>Clear All</span>
+                </button>
             </div>
 
-            <form>
+            <form className={styles.calculator_form}>
                 <label htmlFor="mortgage-amount">Mortgage Amount</label>
                 <input type="number" id="mortgage-amount"></input>
 
@@ -26,7 +28,10 @@ function CalculatorForm() {
                     <label htmlFor="interest-only">Interest Only</label>
                 </fieldset>
 
-                <button type="submit">Calculate Repayments</button>
+                <button className={styles.calculator_form_submit_btn} type="submit">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path fill="#133041" d="M18.75 2.25H5.25a1.5 1.5 0 0 0-1.5 1.5v16.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5V3.75a1.5 1.5 0 0 0-1.5-1.5Zm-10.5 16.5a1.125 1.125 0 1 1 0-2.25 1.125 1.125 0 0 1 0 2.25Zm0-3.75a1.125 1.125 0 1 1 0-2.25 1.125 1.125 0 0 1 0 2.25ZM12 18.75a1.125 1.125 0 1 1 0-2.25 1.125 1.125 0 0 1 0 2.25ZM12 15a1.125 1.125 0 1 1 0-2.25A1.125 1.125 0 0 1 12 15Zm3.75 3.75a1.125 1.125 0 1 1 0-2.25 1.125 1.125 0 0 1 0 2.25Zm0-3.75a1.125 1.125 0 1 1 0-2.25 1.125 1.125 0 0 1 0 2.25Zm1.5-5.25a.75.75 0 0 1-.75.75h-9a.75.75 0 0 1-.75-.75V6a.75.75 0 0 1 .75-.75h9a.75.75 0 0 1 .75.75v3.75Z"/></svg>
+                    <span>Calculate Repayments</span>
+                </button>
             </form>
         </div>
     );
